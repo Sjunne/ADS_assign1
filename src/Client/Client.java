0335532cba2ad8c9cc01aad048805544daa5d122
Sjunne <sjunneson@hotmail.com>
2021-03-18 12:40:17 +0100

    started på unittests

---
+++ src/Client/Client.java
@@ -0,0 +1,62 @@
+package Client;
+
+import Calculator.*;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Client {
+
+    private Token token;
+
+    public int evaluateExpression(ArrayList<Token> tokenList) throws Calculator.MalformedExpressionException {
+        CalculatorVisitor visitor = new CalculatorVisitor();
+
+        for (int i = 0; i < tokenList.size(); i++) {
+            tokenList.get(i).accept(visitor);
+        }
+
+            return visitor.getResult();
+
+    }
+
+    /*
+
+    public static void main(String[] args) {
+        ArrayList<Token> tokenArrayList = new ArrayList<>();
+
+
+        Operand operand1 = new Operand();
+        Operand operand2 = new Operand();
+        Operand operand3 = new Operand();
+        Operand operand4 = new Operand();
+        Operand operand5 = new Operand();
+        Operand operand6 = new Operand();
+        operand1.setValue(0);
+        operand2.setValue(-2);
+        operand3.setValue(3);
+        operand4.setValue(4);
+        operand5.setValue(5);
+        operand6.setValue(6);
+        Operator operator1 = new Operator();
+        Operator operator2 = new Operator();
+        Operator operator3 = new Operator();
+        Operator operator4 = new Operator();
+        operator1.setOperation(Operation.GANGE);
+        operator2.setOperation(Operation.DIVIDERE);
+        operator3.setOperation(Operation.PLUS);
+        operator4.setOperation(Operation.MINUS);
+
+        tokenArrayList.add(operand2);
+        tokenArrayList.add(operand2);
+        tokenArrayList.add(operator3);
+        tokenArrayList.add(operand1);
+        tokenArrayList.add(operator2);
+
+
+        System.out.println(evaluateExpression(tokenArrayList));
+    }
+
+
+     */
+}
---
+++ src/UnitTesting/CalcutorTests.java
@@ -0,0 +1,312 @@
+package UnitTesting;
+
+import Calculator.*;
+import Client.Client;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+public class CalcutorTests {
+    private Calculator calculator;
+    private Client client;
+    private ArrayList<Token> tokenArrayList;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new CalculatorVisitor();
+        client = new Client();
+        tokenArrayList = new ArrayList<>();
+    }
+
+    /**
+     * Easy calculations, with 1 iteration on the stack
+     */
+    @Test
+    void easyCalculationWithAddition()
+    {
+        Operand operand1 = new Operand();
+        Operand operand2 = new Operand();
+        Operator operator1 = new Operator();
+        operand1.setValue(2);
+        operand2.setValue(2);
+        operator1.setOperation(Operation.PLUS);
+
+        tokenArrayList.add(operand1);
+        tokenArrayList.add(operand2);
+        tokenArrayList.add(operator1);
+
+        try {
+            Assertions.assertEquals((operand1.getValue() + operand2.getValue()), client.evaluateExpression(tokenArrayList));
+        } catch (Calculator.MalformedExpressionException e) {
+            e.printStackTrace();
+        }
+    }
+    @Test
+    void easyCalculationWithSubtraction()
+    {
+        Operand operand1 = new Operand();
+        Operand operand2 = new Operand();
+        Operator operator1 = new Operator();
+        operand1.setValue(2);
+        operand2.setValue(2);
+        operator1.setOperation(Operation.MINUS);
+
+        tokenArrayList.add(operand1);
+        tokenArrayList.add(operand2);
+        tokenArrayList.add(operator1);
+
+        try {
+            Assertions.assertEquals((operand1.getValue() - operand2.getValue()), client.evaluateExpression(tokenArrayList));
+        } catch (Calculator.MalformedExpressionException e) {
+            e.printStackTrace();
+        }
+    }
+    @Test
+    void easyCalculationWithDivision()
+    {
+        Operand operand1 = new Operand();
+        Operand operand2 = new Operand();
+        Operator operator1 = new Operator();
+        operand1.setValue(2);
+        operand2.setValue(2);
+        operator1.setOperation(Operation.DIVIDERE);
+
+        tokenArrayList.add(operand1);
+        tokenArrayList.add(operand2);
+        tokenArrayList.add(operator1);
+
+        try {
+            Assertions.assertEquals((operand1.getValue() / operand2.getValue()), client.evaluateExpression(tokenArrayList));
+        } catch (Calculator.MalformedExpressionException e) {
+            e.printStackTrace();
+        }
+    }
+    @Test
+    void easyCalculationWithMultiply()
+    {
+        Operand operand1 = new Operand();
+        Operand operand2 = new Operand();
+        Operator operator1 = new Operator();
+        operand1.setValue(2);
+        operand2.setValue(2);
+        operator1.setOperation(Operation.GANGE);
+
+        tokenArrayList.add(operand1);
+        tokenArrayList.add(operand2);
+        tokenArrayList.add(operator1);
+
+        try {
+            Assertions.assertEquals((operand1.getValue() * operand2.getValue()), client.evaluateExpression(tokenArrayList));
+        } catch (Calculator.MalformedExpressionException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * Easy calculations, with 2 iteration on the stack
+     */
+    @Test
+    void mediumCalculationWithAddition()
+    {
+        Operand operand1 = new Operand();
+        Operand operand2 = new Operand();
+        Operator operator1 = new Operator();
+        Operand operand3 = new Operand();
+        Operator operator2 = new Operator();
+        operand1.setValue(2);
+        operand2.setValue(2);
+        operand3.setValue(4);
+        operator2.setOperation(Operation.PLUS);
+        operator1.setOperation(Operation.PLUS);
+
+        tokenArrayList.add(operand1);
+        tokenArrayList.add(operand2);
+        tokenArrayList.add(operator1);
+        tokenArrayList.add(operand3);
+        tokenArrayList.add(operator2);
+
+        try {
+            Assertions.assertEquals(8, client.evaluateExpression(tokenArrayList));
+        } catch (Calculator.MalformedExpressionException e) {
+            e.printStackTrace();
+        }
+    }
+    @Test
+    void mediumCalculationWithSubtraction()
+    {
+        Operand operand1 = new Operand();
+        Operand operand2 = new Operand();
+        Operator operator1 = new Operator();
+        Operand operand3 = new Operand();
+        Operator operator2 = new Operator();
+        operand1.setValue(2);
+        operand2.setValue(2);
+        operand3.setValue(4);
+        operator2.setOperation(Operation.MINUS);
+        operator1.setOperation(Operation.MINUS);
+
+        tokenArrayList.add(operand1);
+        tokenArrayList.add(operand2);
+        tokenArrayList.add(operator1);
+        tokenArrayList.add(operand3);
+        tokenArrayList.add(operator2);
+
+        try {
+            Assertions.assertEquals(-4, client.evaluateExpression(tokenArrayList));
+        } catch (Calculator.MalformedExpressionException e) {
+            e.printStackTrace();
+        }
+    }
+    @Test
+    void mediumCalculationWithDivision()
+    {
+        Operand operand1 = new Operand();
+        Operand operand2 = new Operand();
+        Operator operator1 = new Operator();
+        Operand operand3 = new Operand();
+        Operator operator2 = new Operator();
+        operand1.setValue(2);
+        operand2.setValue(2);
+        operand3.setValue(4);
+        operator2.setOperation(Operation.DIVIDERE);
+        operator1.setOperation(Operation.DIVIDERE);
+
+        tokenArrayList.add(operand1);
+        tokenArrayList.add(operand2);
+        tokenArrayList.add(operator1);
+        tokenArrayList.add(operand3);
+        tokenArrayList.add(operator2);
+
+        //svaret er egentlig 0.25, men da vi ikke vil ændre i "getResult" så vil den ikke kunne håndtere det
+        try {
+            Assertions.assertEquals(0, client.evaluateExpression(tokenArrayList));
+        } catch (Calculator.MalformedExpressionException e) {
+            e.printStackTrace();
+        }
+    }
+    @Test
+    void mediumCalculationWithMultiply()
+    {
+        Operand operand1 = new Operand();
+        Operand operand2 = new Operand();
+        Operator operator1 = new Operator();
+        Operand operand3 = new Operand();
+        Operator operator2 = new Operator();
+        operand1.setValue(2);
+        operand2.setValue(2);
+        operand3.setValue(4);
+        operator2.setOperation(Operation.GANGE);
+        operator1.setOperation(Operation.GANGE);
+
+        tokenArrayList.add(operand1);
+        tokenArrayList.add(operand2);
+        tokenArrayList.add(operator1);
+        tokenArrayList.add(operand3);
+        tokenArrayList.add(operator2);
+
+        try {
+            Assertions.assertEquals(16, client.evaluateExpression(tokenArrayList));
+        } catch (Calculator.MalformedExpressionException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void calculationWithMultipleOperands()
+    {
+        Operand operand1 = new Operand();
+        Operand operand2 = new Operand();
+        Operator operator1 = new Operator();
+        Operand operand3 = new Operand();
+        Operator operator2 = new Operator();
+        operand1.setValue(2);
+        operand2.setValue(2);
+        operand3.setValue(4);
+        operator1.setOperation(Operation.DIVIDERE);
+        operator2.setOperation(Operation.GANGE);
+
+        tokenArrayList.add(operand1);
+        tokenArrayList.add(operand2);
+        tokenArrayList.add(operator1);
+        tokenArrayList.add(operand3);
+        tokenArrayList.add(operator2);
+
+        try {
+            Assertions.assertEquals(4, client.evaluateExpression(tokenArrayList));
+        } catch (Calculator.MalformedExpressionException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void calculationWithMultipleOperands2()
+    {
+        Operand operand1 = new Operand();
+        Operand operand2 = new Operand();
+        Operator operator1 = new Operator();
+        Operand operand3 = new Operand();
+        Operator operator2 = new Operator();
+        Operand operand4 = new Operand();
+        Operator operator3 = new Operator();
+        Operand operand5 = new Operand();
+        Operator operator4 = new Operator();
+        operand1.setValue(2);
+        operand2.setValue(2);
+        operand3.setValue(4);
+        operand4.setValue(10);
+        operand5.setValue(16);
+
+        operator1.setOperation(Operation.DIVIDERE);
+        operator2.setOperation(Operation.GANGE);
+        operator3.setOperation(Operation.MINUS);
+        operator4.setOperation(Operation.PLUS);
+
+        tokenArrayList.add(operand1);
+        tokenArrayList.add(operand2);
+        tokenArrayList.add(operator1);
+        tokenArrayList.add(operand3);
+        tokenArrayList.add(operator2);
+        tokenArrayList.add(operand4);
+        tokenArrayList.add(operator3);
+        tokenArrayList.add(operand5);
+        tokenArrayList.add(operator4);
+
+
+        try {
+            Assertions.assertEquals(10, client.evaluateExpression(tokenArrayList));
+        } catch (Calculator.MalformedExpressionException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void MalformedExpression1()
+    {
+        Operand operand1 = new Operand();
+        Operand operand2 = new Operand();
+        Operator operator1 = new Operator();
+        Operand operand3 = new Operand();
+        Operand operand4 = new Operand();
+        operand1.setValue(1);
+        operand2.setValue(1);
+        operator1.setOperation(Operation.MINUS);
+        operand3.setValue(1);
+        operand4.setValue(1);
+        Operator operator2 = new Operator();
+        operator2.setOperation(Operation.MINUS);
+
+        tokenArrayList.add(operand1);
+        tokenArrayList.add(operand2);
+        tokenArrayList.add(operator1);
+        tokenArrayList.add(operand3);
+        tokenArrayList.add(operand4);
+        tokenArrayList.add(operator2);
+
+        Assertions.assertThrows(Calculator.MalformedExpressionException.class, () ->
+            client.evaluateExpression(tokenArrayList));
+        }
+    }
+
